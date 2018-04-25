18af6c9a8b06377c087c02ce80f8e7c2a9f73bbc
Nikita Alekseev <n.alekseev2104@gmail.com>
2018-04-25 21:45:15 +0300

    Add ready handler

--- main.go
+++ main.go
@@ -12,14 +12,19 @@ var (
 	last_lobby_id int64
 )
 
+type Player struct {
+	Address string	`json:"address"`
+	Ready bool	`json:"ready"`
+}
+
 type Lobby struct {
 	ID int64	`json:"id"`
 	PlayersCount int `json:"players_count"`
-	PlayerIPs []string `json:"players"`
+	Players []Player `json:"players"`
 }
 
 func createLobby(w http.ResponseWriter, r *http.Request) {
-	lobby := Lobby{last_lobby_id, 0, []string{}}
+	lobby := Lobby{last_lobby_id, 0, []Player{}}
 	lobbies[last_lobby_id] = lobby
 
 	atomic.AddInt64(&last_lobby_id, 1)
@@ -96,13 +101,13 @@ func leaveLobby(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	i := 0
-	for lobby.PlayerIPs[i] != address[0] && i < len(lobby.PlayerIPs) {
+	for lobby.Players[i].Address != address[0] && i < len(lobby.Players) {
 		i++
 	}
-	if i == len(lobby.PlayerIPs) {
+	if i == len(lobby.Players) {
 		w.Write([]byte("No IP found"))
 	}
-	lobbies[lobby_id] = Lobby{lobby_id, lobby.PlayersCount - 1, append(lobby.PlayerIPs[:i], lobby.PlayerIPs[i+1:]...)}
+	lobbies[lobby_id] = Lobby{lobby_id, lobby.PlayersCount - 1, append(lobby.Players[:i], lobby.Players[i+1:]...)}
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Removed from lobby"))
 }
@@ -130,7 +135,12 @@ func joinLobby(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Lobby does not exist"))
 		return
 	}
-	lobbies[lobby_id] = Lobby{lobby_id, lobby.PlayersCount + 1, append(lobby.PlayerIPs, address[0])}
+	if lobby.PlayersCount == 4 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Lobby is full"))
+		return
+	}
+	lobbies[lobby_id] = Lobby{lobby_id, lobby.PlayersCount + 1, append(lobby.Players, Player{address[0], false})}
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Added to lobby"))
 }
@@ -165,6 +175,52 @@ func deleteLobby(w http.ResponseWriter, r *http.Request) {
 	delete(lobbies, lobby_id)
 }
 
+func playerReady(w http.ResponseWriter, r *http.Request) {
+	lid, ok := r.URL.Query()["lobby_id"]
+	if !ok {
+		w.Write([]byte("No lobby_id key"))
+		return
+	}
+
+	address, ok := r.URL.Query()["address"]
+	if !ok {
+		w.Write([]byte("No address key"))
+		return
+	}
+
+	lobby_id, err := strconv.ParseInt(lid[0], 10, 32)
+	if err != nil {
+		w.Write([]byte("lobby_id must be valid int"))
+		return
+	}
+	lobby, ok := lobbies[lobby_id]
+	if !ok {
+		w.Write([]byte("Lobby does not exist"))
+		return
+	}
+	if lobby.PlayersCount < 4 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Lobby is not full"))
+		return
+	}
+	for i := 0; i < len(lobby.Players); i++ {
+		if lobby.Players[i].Address == address[0] {
+			lobby.Players[i].Ready = true
+			break
+		}
+	}
+
+	lobbies[lobby_id] = lobby
+	json, err := json.Marshal(&lobby)
+
+	if err != nil {
+		w.Write([]byte("Internal server error"))
+		return
+	}
+
+	w.Write(json)
+}
+
 func main() {
 	atomic.StoreInt64(&last_lobby_id, 0)
 	lobbies = make(map[int64]Lobby)
@@ -174,6 +230,7 @@ func main() {
 	http.HandleFunc("/lobby", getLobby)
 	http.HandleFunc("/delete", deleteLobby)
 	http.HandleFunc("/leave", leaveLobby)
+	http.HandleFunc("/ready", playerReady)
 
 	http.ListenAndServe(":8080", nil)
 }
